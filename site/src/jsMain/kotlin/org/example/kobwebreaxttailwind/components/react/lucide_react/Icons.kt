8cb30f93c28a4ff40c049660c326d01c0c736983
Kizzy <vaibhavmonu8309@gmil.com>
2023-06-24 08:54:47 +0000

    add lucide icons file

---
+++ site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/components/react/lucide_react/Icons.kt
@@ -0,0 +1,67 @@
+@file:JsModule("lucide-react")
+@file:JsNonModule
+
+package org.example.kobwebreaxttailwind.components.react.lucide_react
+
+import react.FC
+
+
+@JsName("Activity")
+external val Activity: FC<LucideProps>
+
+@JsName("CreditCard")
+external val CreditCard: FC<LucideProps>
+
+@JsName("DollarSign")
+external val DollarSign: FC<LucideProps>
+
+@JsName("Download")
+external val Download: FC<LucideProps>
+
+@JsName("Users")
+external val Users: FC<LucideProps>
+
+@JsName("LogOut")
+external val LogOut: FC<LucideProps>
+
+@JsName("PlusCircle")
+external val PlusCircle: FC<LucideProps>
+
+@JsName("Settings")
+external val Settings: FC<LucideProps>
+
+@JsName("User")
+external val User: FC<LucideProps>
+
+@JsName("BellRing")
+external val BellRing: FC<LucideProps>
+
+@JsName("Check")
+external val Check: FC<LucideProps>
+
+@JsName("Loader2")
+external val Loader2: FC<LucideProps>
+
+@JsName("Mail")
+external val Mail: FC<LucideProps>
+
+@JsName("Keyboard")
+external val Keyboard: FC<LucideProps>
+
+@JsName("UserPlus")
+external val UserPlus: FC<LucideProps>
+
+@JsName("MessageSquare")
+external val MessageSquare: FC<LucideProps>
+
+@JsName("Plus")
+external val Plus: FC<LucideProps>
+
+@JsName("Github")
+external val Github: FC<LucideProps>
+
+@JsName("LifeBuoy")
+external val LifeBuoy: FC<LucideProps>
+
+@JsName("Cloud")
+external val Cloud: FC<LucideProps>
