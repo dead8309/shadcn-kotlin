7c83f4ea6ac83ca4b2f3261aa625df3d63ecd1ed
Vaibhav <vaibhavmonu8309@gmail.com>
2023-06-30 05:50:25 +0000

    fix: dark theme not working

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -2,7 +2,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ["class","[data-theme='dark']"],
   content: [
   "**/*.{html,js}",
   "../../node_modules/@lshay/ui/dist/components/**/*.jsx",
