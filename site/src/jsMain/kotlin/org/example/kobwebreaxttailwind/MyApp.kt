c1388e380f6fbd82097168bb05da762aa9775fb4
Vaibhav <vaibhavmonu8309@gmail.com>
2023-06-30 14:02:03 +0000

    Improved page layout

--- site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/MyApp.kt
+++ site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/MyApp.kt
@@ -1,48 +1,32 @@
 package org.example.kobwebreaxttailwind
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
-import com.varabyte.kobweb.compose.ui.modifiers.minHeight
 import com.varabyte.kobweb.core.App
-import com.varabyte.kobweb.silk.SilkApp
-import com.varabyte.kobweb.silk.components.layout.Surface
-import com.varabyte.kobweb.silk.components.style.common.SmoothColorStyle
-import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
 import com.varabyte.kobweb.silk.init.registerBaseStyle
-import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import com.varabyte.kobweb.silk.theme.colors.getColorMode
-import kotlinx.browser.localStorage
-import org.jetbrains.compose.web.css.vh
-
-private const val COLOR_MODE_KEY = "kobwebreaxttailwind:colorMode"
+import org.jetbrains.compose.web.dom.Div
 
 @InitSilk
 fun initSilk(ctx: InitSilkContext) {
-    ctx.config.initialColorMode = localStorage.getItem(COLOR_MODE_KEY)?.let { ColorMode.valueOf(it) } ?: ColorMode.LIGHT
-
     ctx.stylesheet.registerBaseStyle("body") {
-        Modifier.fontFamily(
-            "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
-            "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"
-        )
+        Modifier
+                .fontFamily(
+                        "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
+                        "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"
+                )
     }
 }
 
 @App
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
-    SilkApp {
-        val colorMode = getColorMode()
-        LaunchedEffect(colorMode) {
-            localStorage.setItem(COLOR_MODE_KEY, colorMode.name)
-        }
-
-        Surface(SmoothColorStyle.toModifier().minHeight(100.vh)) {
-            content()
-        }
+    Div(attrs = {
+        classes("w-full", "h-full", "min-h-screen", "bg-background", "antialiased")
+        id("app")
+    }) {
+        content()
     }
 }
--- site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/components/layouts/PageLayout.kt
+++ site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/components/layouts/PageLayout.kt
@@ -1,45 +1,47 @@
 package org.example.kobwebreaxttailwind.components.layouts
 
-import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.css.*
-import com.varabyte.kobweb.compose.foundation.layout.Box
-import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.ui.Alignment
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.*
-import kotlinx.browser.document
-import org.jetbrains.compose.web.css.*
-import org.jetbrains.compose.web.dom.*
-import org.example.kobwebreaxttailwind.components.sections.Footer
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import org.example.kobwebreaxttailwind.components.sections.NavHeader
+import org.example.kobwebreaxttailwind.theme.ThemeProvider
+import react.FC
+import react.Props
+import react.ReactNode
+import react.create
+import react.dom.client.createRoot
+import react.dom.html.ReactHTML.div
+import web.cssom.ClassName
+import web.dom.document
+
 
 @Composable
-fun PageLayout(title: String, content: @Composable () -> Unit) {
-    LaunchedEffect(title) {
-        document.title = title
+fun PageLayout(
+        title: String,
+        children: ReactNode? = null
+) {
+    kotlinext.js.require("./globals.css")
+    val layout = FC<Props> {
+        ThemeProvider {
+            // attribute = "class"
+            defaultTheme = "system"
+            enableSystem = true
+            div {
+                className = ClassName("relative flex min-h-screen flex-col")
+                NavHeader {}
+                div {
+                    className = ClassName("flex-1")
+                    +children
+                }
+                // SiteFooter
+            }
+        }
+        // Toaster
     }
 
-    Box(Modifier
-        .fillMaxWidth()
-        .minHeight(100.percent)
-        // Create a box with two rows: the main content (fills as much space as it can) and the footer (which reserves
-        // space at the bottom). "min-content" means the use the height of the row, which we use for the footer. "1fr"
-        // means give the rest of the space to that row, and "minmax(0, 1fr)" is a CSS trick which essentially means
-        // the same thing but can avoid a measurement issue if you end up nesting additional grids inside of it. (See
-        // also: https://github.com/w3c/csswg-drafts/issues/1777) Since this box is set to *at least* 100%, the footer
-        // will always appear at least on the bottom but can be pushed further down if the first row grows beyond the
-        // page.
-        .gridTemplateRows("minmax(0, 1fr) min-content")
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize().textAlign(TextAlign.Center),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            NavHeader()
-            H1 { Text(title) }
-            content()
-        }
-        // Associate the footer with the row that will get pushed off the bottom of the page if it can't fit.
-        Footer(Modifier.align(Alignment.Center).gridRowStart(2).gridRowEnd(3))
+
+    LaunchedEffect(Unit) {
+        document.title = title
+        val root = document.getElementById("app")
+        createRoot(root!!).render(layout.create())
     }
 }
