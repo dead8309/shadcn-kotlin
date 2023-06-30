0ab76676d3bb10a6853e54bc66688e8028718e7c
Vaibhav <vaibhavmonu8309@gmail.com>
2023-06-30 22:22:03 +0000

    fix fonts not loading

--- site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/MyApp.kt
+++ site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/MyApp.kt
@@ -1,31 +1,19 @@
 package org.example.kobwebreaxttailwind
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
-import com.varabyte.kobweb.silk.init.registerBaseStyle
 import org.jetbrains.compose.web.dom.Div
 
 @InitSilk
-fun initSilk(ctx: InitSilkContext) {
-    ctx.stylesheet.registerBaseStyle("body") {
-        Modifier
-                .fontFamily(
-                        "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
-                        "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"
-                )
-    }
-}
-
+fun initSilk(ctx: InitSilkContext) {}
 @App
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     Div(attrs = {
-        classes("w-full", "h-full", "min-h-screen", "bg-background", "antialiased")
-        id("app")
+            classes( "min-h-screen", "antialiased")
+            id("app")
     }) {
         content()
     }
--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -58,7 +58,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["Inter", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
