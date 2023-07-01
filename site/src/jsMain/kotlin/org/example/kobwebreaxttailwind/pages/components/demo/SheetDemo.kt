5faf79dd1799624923980406980057bbe21d036f
Vaibhav <vaibhavmonu8309@gmail.com>
2023-07-01 08:41:09 +0000

    fix SheetDemo

--- site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/pages/components/demo/SheetDemo.kt
+++ site/src/jsMain/kotlin/org/example/kobwebreaxttailwind/pages/components/demo/SheetDemo.kt
@@ -3,70 +3,61 @@ package org.example.kobwebreaxttailwind.pages.components.demo
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.div
-import react.dom.html.ReactHTML.h4
 import shadcn.ui.components.*
 import web.cssom.ClassName
-import web.html.ButtonType
 
 val SheetDemo = FC<Props> {
-    h4 {
-        className = ClassName("text-sm text-destructive")
-        + "Currently broken"
-    }
-    /*
-Sheet {
-    SheetTrigger {
-        asChild = true
-        Button {
-            variant = "outline"
-            +"Open"
-        }
-    }
-    SheetContent {
-        position = "right"
-        size = "sm"
-        shadcn.ui.components.SheetHeader {
-            shadcn.ui.components.SheetTitle {
-                +"Edit profile"
-            }
-            shadcn.ui.components.SheetDescription {
-                +"Make changes to your profile here . Click save when you 're done."
+    Sheet {
+        SheetTrigger {
+            asChild = true
+            Button {
+                variant = "outline"
+                +"Open"
             }
         }
-        react.dom.html.ReactHTML.div {
-            className = web.cssom.ClassName("grid gap-4 py-4")
-            react.dom.html.ReactHTML.div {
-                className = web.cssom.ClassName("grid grid-cols-4 items-center gap-4")
-                shadcn.ui.components.Label {
-                    htmlFor = "name"
-                    className = web.cssom.ClassName("text-right")
-                    +"Name"
+        SheetContent {
+            SheetHeader {
+                SheetTitle {
+                    +"Edit profile"
                 }
-
-            }
-            react.dom.html.ReactHTML.div {
-                className = web.cssom.ClassName("grid grid-cols-4 items-center gap-4")
-                shadcn.ui.components.Label {
-                    htmlFor = "username"
-                    className = web.cssom.ClassName("text-right")
-                    +"Username"
-                }
-                shadcn.ui.components.Input {
-                    id = "username"
-                    value = "@peduarte"
-                    className = web.cssom.ClassName("col-span-3")
+                SheetDescription {
+                    +"Make changes to your profile here . Click save when you 're done."
                 }
             }
-        }
-        shadcn.ui.components.SheetFooter {
-            shadcn.ui.components.SheetClose {
-                asChild = true
-                shadcn.ui.components.Button {
-                    type = web.html.ButtonType.submit
-                    +"Save changes"
+            div {
+                className = ClassName("grid gap-4 py-4")
+                div {
+                    className = ClassName("grid grid-cols-4 items-center gap-4")
+                    Label {
+                        htmlFor = "name"
+                        className = ClassName("text-right")
+                        +"Name"
+                    }
+                    Input {
+                        id = "name"
+                        defaultValue = "Pedro Duarte"
+                        className = ClassName("col-span-3")
+                    }
+                }
+                div {
+                    className = ClassName("grid grid-cols-4 items-center gap-4")
+                    Label {
+                        htmlFor = "username"
+                        className = ClassName("text-right")
+                        +"Username"
+                    }
+                    Input {
+                        id = "username"
+                        defaultValue = "@peduarte"
+                        className = ClassName("col-span-3")
+                    }
+                }
+            }
+            SheetFooter {
+                Button {
+                    +"Save Changes"
                 }
             }
         }
     }
-}*/
 }
