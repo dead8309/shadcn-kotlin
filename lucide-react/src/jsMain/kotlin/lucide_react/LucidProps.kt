cc8cd4502588e7ae9ba996648769ce1f7631f047
Vaibhav <vaibhavmonu8309@gmail.com>
2023-07-06 21:20:55 +0000

    use lucide-kotlin library

--- lucide-react/src/jsMain/kotlin/lucide_react/LucidProps.kt
+++
@@ -1,9 +0,0 @@
-package lucide_react
-
-import react.dom.svg.SVGAttributes
-import web.svg.SVGElement
-
-external interface LucideProps : SVGAttributes<SVGElement> {
-    var size: String?
-    var absoluteStrokeWidth: Boolean
-}
