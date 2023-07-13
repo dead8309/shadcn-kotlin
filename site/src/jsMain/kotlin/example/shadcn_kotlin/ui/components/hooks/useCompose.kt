3c6c4baf9c2749c0a0c88381db6512c0cdba5d26
Vaibhav <vaibhavmonu8309@gmail.com>
2023-07-13 18:35:18 +0000

    refactor: id from useCompose will be set by randomUUID if not specified

--- site/src/jsMain/kotlin/example/shadcn_kotlin/ui/components/hooks/useCompose.kt
+++ site/src/jsMain/kotlin/example/shadcn_kotlin/ui/components/hooks/useCompose.kt
@@ -3,13 +3,14 @@ package example.shadcn_kotlin.ui.components.hooks
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.web.renderComposable
 import react.useLayoutEffectOnce
+import web.crypto.crypto
 
 fun useCompose(
-    id: String,
+    id: String = "",
     composable: @Composable () -> Unit
 ) {
     useLayoutEffectOnce {
-        renderComposable(id) {
+        renderComposable(id.ifEmpty { crypto.randomUUID() }) {
             composable()
         }
     }
